d22e63dd4609537b20e78b9572267c6ccd84e272
Pranay Gundu <pranaygundu423@gmail.com>
2025-07-16 09:39:55 +0530

    first commit

---
+++ src/data_ingestion.py
@@ -0,0 +1,20 @@
+# load data from url
+
+#url->changes->local_copy
+
+import os
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
+# delete tweet id
+df.drop(columns=['tweet_id'],inplace=True)
+final_df = df[df['sentiment'].isin(['happiness','sadness'])]
+final_df['sentiment'].replace({'happiness':1, 'sadness':0},inplace=True)
+train_data, test_data = train_test_split(final_df, test_size=0.2, random_state=42)
+
+data_path = os.path.join("data","raw")
+os.makedirs(data_path)
+train_data.to_csv(os.path.join(data_path , "train.csv"))
+test_data.to_csv(os.path.join(data_path , "test.csv"))
